16cd460b13e915795a8c72b0acb2a19464f9d85b
Fika98 <Afzalov98@gmail.com>
2020-01-28 17:21:04 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+    # def name
+    #     @name
+    # end
+
+    # def breed
+    #     @breed
+    # end
+    def name=(value)
+        @name = value
+    end
+def breed=(value)
+        @breed = value
+end
+def name
+    @name
+end
+
+def breed
+    @breed
+end
+
+
+end 
+
